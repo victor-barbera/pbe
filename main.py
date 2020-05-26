7ab9f67d02f984b6ed1b881136c760f51c7035ad
feymery <marinamiro.99@gmail.com>
2020-05-26 14:29:43 +0200

    canvi estilitzat i lcd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import gi, threading, time, requests, json#, nfcReader#, i2c !s'ha de treure el json
+import gi, threading, time, requests, json, RPi_I2C_driver #, nfcReader
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk,Gdk
 
@@ -33,12 +33,10 @@ class Window(Gtk.Window) :
         res = requests.get("http://188.166.21.177:5000/" + query)
 #        rows = '{"tableName" : "timetables", "rows" : [["div","8:00","DSBM","a4007"],["div","8:00","DSBM","a4007"],["div","8:00","DSBM","a4007"]]}'
 #        rowsj = json.loads(rows)
-        print("http://188.166.21.177:5000/" + query)
-        print(res.json())
+#        print("http://188.166.21.177:5000/" + query)
         if(table): self.query.createTable(res.json()["tableName"],res.json()["rows"])
         else:
             global user
-            #user["name"] = "null"
             user["name"] = res.json()["name"]
             self.query.studentName.set_text(user['name'])
 
@@ -56,36 +54,37 @@ class Login(Gtk.Box):
         self.pack_start(self.vBox, True, False, 50)
         
         self.login=Gtk.Button(label="Please, login with your university card")
+        self.l=RPi_I2C_driver.lcd()
+        self.l.lcd_display_string_pos("Please, login with",2,1)
+        self.l.lcd_display_string("your university card",3)
         self.entry = Gtk.Entry()
         self.entry.connect("activate", self.onProva)
+        self.vBox.pack_start(self.entry,True,False,0)
         
         self.login.set_name("login")
         self.login.set_property("width-request", 200)
         self.login.set_property("height-request", 50)
-        self.login.connect("clicked", self.onProva)#per fer proves sense nfc
         self.vBox.pack_start(self.login,True,False,0)
-        self.vBox.pack_start(self.entry,True,False,0)
-
+        
         #aixo quan hi ha el nfc no es posa
         self.button = Gtk.Button(label="Error")
         self.button.connect("clicked", self.onError)
         self.vBox.pack_start(self.button,True,True,6)
         #threading.Thread(target=self.nfcThread, daemon=True).start()
         
-        
-        
-        
     def onProva(self, widget) :
         user["uid"] = self.entry.get_text()
         self.parent_window.httpThread("login?student_id=" + user["uid"], False)
         if(user["name"] != "null") :
+            self.l.lcd_clear()
+            self.l.lcd_display_string_pos("Welcome",2,6)
+            self.l.lcd_display_string_pos(user["name"],3,3)
             self.hide()
             self.parent_window.query.show_all()
         else:
             self.login.set_label("Your ID is not in our list. Please try again")
             self.login.set_name("loginError")
         #això és per fer proves i s'haurà de canviar per la lectura del nfc
-#      i2c.lcd_display_string("Welcome"+student_name)
 #         self.hide()
 #         self.parent_window.query.show_all()
 #         thread = threading.Thread(target=self.parent_window.httpThread, args=["login?id=" + self.uid , False])
@@ -121,24 +120,31 @@ class Query(Gtk.Box):#aqui tot per fer consultes
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL, spacing=30)
         self.parent_window = parent_window
         
+        
         self.hBox = Gtk.HBox(spacing=5)
         self.add(self.hBox)
         self.label = Gtk.Label(label="Welcome", xalign=0)
         self.studentName = Gtk.Label(label=user['name'])
-        self.studentName.set_name('studentName')
+        self.studentName.set_name('Name')
         self.hBox.pack_start(self.label, False, True, 0)
         self.hBox.pack_start(self.studentName, False, True, 0)
+        
         self.button = Gtk.Button(label="Log Out")
         self.button.connect("clicked", self.onLogOut)
         self.hBox.add(self.button)
         self.hBox.set_child_packing(self.button,False,True,0,1)
+        
         self.entry = Gtk.Entry()
-        self.entry.set_has_frame(True)
-        #self.entry.set_text("table?constraint&constraint..")#aqui podem ficar el format que volem
         self.add(self.entry)
         self.entry.connect("activate", self.processQuery)
+        
+        self.view = Gtk.TreeView()
+        self.table=Gtk.Label()
     
     def createTable(self, tableName, rows) :
+        self.table = Gtk.Label(label=tableName)
+        self.table.set_name("Name")
+        self.add(self.table)
         if(tableName == "timetables") :
             columns = ["day", "hour", "subject", "room"]
             listmodel = Gtk.ListStore(str, str, str, str)
@@ -148,36 +154,30 @@ class Query(Gtk.Box):#aqui tot per fer consultes
         if(tableName == "marks"):
             columns = ["subject", "name", "mark"]
             listmodel = Gtk.ListStore(str, str, float)
-#             {"tableName" : "timetables",
-#              "rows" : [
-#                 ["icom","guifre","4"],
-#                 ["dsbm", "victor", "8"]
-#             ]}
         # append the values in the model
         for i in range(len(rows)):
             listmodel.append(rows[i])
+#            if(i%2==0):rows[i].set_name("parells")
+#            else rows[i].set_name("imparells")
         # a treeview to see the data stored in the model
         self.view = Gtk.TreeView(model=listmodel)
-        print(self.view)
         self.view.set_hexpand(True)
+        self.view.set_grid_lines(1)
         # for each column
-        for i, column in enumerate(columns):
+        for i in range(len(columns)):
             # cellrenderer to render the text
             cell = Gtk.CellRendererText()
             # the column is created
-            col = Gtk.TreeViewColumn(column, cell, text=i)
+            col = Gtk.TreeViewColumn(columns[i], cell, text=i)
             col.set_expand(True)
             # and it is appended to the treeview
             self.view.append_column(col)
-        self.grid = Gtk.Grid()
-        self.add(self.grid)
-        self.grid.attach(self.view, 0, 0, 1, 1)
-        
-        #grid.attach(self.label, 0, 1, 1, 1)   
-        self.grid.show_all()
-        
-        
+        self.add(self.view)
+        self.show_all()
+                
     def processQuery(self, widget):
+        self.remove(self.view)
+        self.remove(self.table)
         #thread = threading.Thread(target=self.parent_window.httpThread, args=[self.entry.get_text(), True])
         self.parent_window.httpThread(self.entry.get_text(), True)
         #thread.daemon = True
